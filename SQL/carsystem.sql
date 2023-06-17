c0f8c2fc3a52a7ba45c277401468804bd5abc3df
Luminescence <2306786764@qq.com>
2023-06-17 22:00:24 +0800

    sql语句修改

--- SQL/carsystem.sql
+++ SQL/carsystem.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50736
  File Encoding         : 65001
 
- Date: 17/06/2023 17:24:24
+ Date: 17/06/2023 21:59:12
 */
 
 SET NAMES utf8mb4;
@@ -35,18 +35,6 @@ CREATE TABLE `car`  (
 ) ENGINE = InnoDB AUTO_INCREMENT = 11 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Records of car
--- ----------------------------
-INSERT INTO `car` VALUES (3, 1, '宝马', '描述', 'K11223', '黑色', 0);
-INSERT INTO `car` VALUES (4, 5, '奥迪', '你好呀', 'A11122', '白色', 0);
-INSERT INTO `car` VALUES (5, 1, '奥迪', '这是一个奥迪', 'A23423', '红色', 0);
-INSERT INTO `car` VALUES (6, 6, '红旗', '这是一个红旗', 'BB3423', '红色', 0);
-INSERT INTO `car` VALUES (7, 7, '劳斯莱斯', '非常干净', 'JK1235', '白色', 0);
-INSERT INTO `car` VALUES (8, 8, '奔驰', '有点脏脏的', 'K9M30', '黑色', 0);
-INSERT INTO `car` VALUES (9, 10, '本田', '崭新出厂', 'M10A1', '银色', 0);
-INSERT INTO `car` VALUES (10, 1, '路虎', '崭新出厂', '京A29B32', '黑色', 0);
-
--- ----------------------------
 -- Table structure for component
 -- ----------------------------
 DROP TABLE IF EXISTS `component`;
@@ -60,17 +48,6 @@ CREATE TABLE `component`  (
   `version` int(255) NOT NULL DEFAULT 0 COMMENT '乐观锁',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of component
--- ----------------------------
-INSERT INTO `component` VALUES (1, 50, 20.00, 40.00, '小装饰A', 0, 1);
-INSERT INTO `component` VALUES (2, 80, 200.00, 300.00, '轮胎A', 0, 0);
-INSERT INTO `component` VALUES (3, 500, 300.00, 350.00, '车漆B', 0, 0);
-INSERT INTO `component` VALUES (4, 20, 200.00, 300.00, '火花塞B', 0, 0);
-INSERT INTO `component` VALUES (5, 139, 50.00, 200.00, '后视镜A', 0, 0);
-INSERT INTO `component` VALUES (6, 139, 60.00, 300.00, '后视镜B', 0, 0);
-INSERT INTO `component` VALUES (7, 80, 10.00, 500.00, '小飞棍', 0, 0);
 
 -- ----------------------------
 -- Table structure for order_component
@@ -87,32 +64,6 @@ CREATE TABLE `order_component`  (
   CONSTRAINT `order_component_ibfk_1` FOREIGN KEY (`orderId`) REFERENCES `theorder` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `order_component_ibfk_2` FOREIGN KEY (`comId`) REFERENCES `component` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE = InnoDB AUTO_INCREMENT = 48 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of order_component
--- ----------------------------
-INSERT INTO `order_component` VALUES (1, 1, 2, 1);
-INSERT INTO `order_component` VALUES (2, 1, 3, 1);
-INSERT INTO `order_component` VALUES (3, 1, 6, 1);
-INSERT INTO `order_component` VALUES (4, 2, 2, 0);
-INSERT INTO `order_component` VALUES (5, 2, 7, 0);
-INSERT INTO `order_component` VALUES (7, 3, 7, 0);
-INSERT INTO `order_component` VALUES (8, 13, 1, 0);
-INSERT INTO `order_component` VALUES (9, 13, 2, 0);
-INSERT INTO `order_component` VALUES (10, 13, 3, 0);
-INSERT INTO `order_component` VALUES (11, 14, 4, 0);
-INSERT INTO `order_component` VALUES (12, 14, 6, 0);
-INSERT INTO `order_component` VALUES (33, 1, 2, 1);
-INSERT INTO `order_component` VALUES (34, 1, 7, 1);
-INSERT INTO `order_component` VALUES (35, 1, 1, 1);
-INSERT INTO `order_component` VALUES (36, 1, 2, 0);
-INSERT INTO `order_component` VALUES (37, 1, 7, 0);
-INSERT INTO `order_component` VALUES (38, 1, 1, 0);
-INSERT INTO `order_component` VALUES (39, 1, 5, 0);
-INSERT INTO `order_component` VALUES (40, 1, 6, 0);
-INSERT INTO `order_component` VALUES (45, 16, 4, 0);
-INSERT INTO `order_component` VALUES (46, 16, 5, 0);
-INSERT INTO `order_component` VALUES (47, 16, 6, 0);
 
 -- ----------------------------
 -- Table structure for theorder
@@ -133,16 +84,6 @@ CREATE TABLE `theorder`  (
 ) ENGINE = InnoDB AUTO_INCREMENT = 17 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Records of theorder
--- ----------------------------
-INSERT INTO `theorder` VALUES (1, 3, '零件是', '2023-06-17 13:59:50', 0, 500.00, 0, 0);
-INSERT INTO `theorder` VALUES (2, 5, '换轮胎', '2023-06-17 13:58:42', 0, 3001.00, 0, 0);
-INSERT INTO `theorder` VALUES (3, 9, '保养', '2023-06-16 14:44:16', 0, 200.00, 0, 0);
-INSERT INTO `theorder` VALUES (13, 3, '我是傻呗', '2023-06-17 13:59:43', 0, 199.00, 0, 0);
-INSERT INTO `theorder` VALUES (14, 5, '测试一下', '2023-06-16 19:36:17', 0, 600.00, 0, 0);
-INSERT INTO `theorder` VALUES (16, 10, '路虎再次测试', '2023-06-17 17:10:48', 0, 800.00, 0, 0);
-
--- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
@@ -160,24 +101,5 @@ CREATE TABLE `user`  (
   UNIQUE INDEX `username`(`username`) USING BTREE,
   INDEX `id`(`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 20 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of user
--- ----------------------------
-INSERT INTO `user` VALUES (1, 2, 'miumiu', '$2a$10$fBL3qGFFR3GJhvzlYUSa6.TYgNVZR8zsi9h68AKYNIMLwfNeLAvWu', '缪缪', '133', '女', '0', 0);
-INSERT INTO `user` VALUES (5, 1, 'aaa', '$2a$10$Vl2knhxEAjNPjA5dYwmxQ.bQGHpxvAGTjN9j4G.zg/Zs8qMxliJ66', '你好', '133', '男', '0', 0);
-INSERT INTO `user` VALUES (6, 0, 'abc', '$2a$10$8Eda64BeILUyOaUnyJWg.e5heTYBTYVQ1r/8ynjZzF7RV/lnClURK', '测试', '11', '女', '1', 1);
-INSERT INTO `user` VALUES (7, 1, 'yf', '$2a$10$5xlb7Am1iyQYHGiCBvNbA.j.dUORDQawOeT/dKqVtN1DlNLLoFiZe', '阳帆', '12', '男', '1', 0);
-INSERT INTO `user` VALUES (8, 1, 'pwb', '$2a$10$95ZyJ7Nj5PnJY9refqO3FeILMRkZLq.6R07ApRlx82bdo13MV6Bv2', '李四1', '13', '男', '0', 0);
-INSERT INTO `user` VALUES (9, 1, 'a', '$2a$10$XkOAbqPr1bcydDOdhYRKA.3rdbTx5A7QfrSuytqF3ZWR.D6suG1zS', '爱你哦', '22', '女', '1', 0);
-INSERT INTO `user` VALUES (10, 0, 'aa', '$2a$10$k4C4.KMRyV8Or4qslclyWuCaoRhwNJbuaXEguLSopsCb1qCWOVZie', '两个aa', '32', '女', '0', 0);
-INSERT INTO `user` VALUES (12, 0, 'b', '$2a$10$iKDFA521iU/fNLjCOacPLuaX77eqtn47FSfxFGZXyObH0BIHmadvO', '一个b', '2523', '男', '0', 0);
-INSERT INTO `user` VALUES (13, 0, 'bb', '$2a$10$POBAID.MKrRdfmYBO2ooduodjy8PQqw/eEGQq6p70PQ.OMKWjr7Du', '两个bb', '432', '男', '0', 0);
-INSERT INTO `user` VALUES (14, 0, 'bbb', '$2a$10$pVhgCFOQBoplYfcwKnwNG.VGEiBkJHVji.nmr9dORmB/1Gcg.UgKO', '三个bbb', '123', '男', '2', 0);
-INSERT INTO `user` VALUES (15, 0, '张三', '$2a$10$RGzpMnIjDJEPhzPVoKsLm.IlCTL3oPBwbxej.AvmYsaDZSQNpAt0u', '张三', '423', '男', '2', 0);
-INSERT INTO `user` VALUES (16, 0, '王二麻子', '$2a$10$moIal0Co8sF8.Np3e/AKGez2ChAqbI1YCtOtRdfoT9zl1pVm9SODO', '王二', '235', '女', '2', 0);
-INSERT INTO `user` VALUES (17, 0, '隔壁老王', '$2a$10$QzI0oUi38ly6i5.7Q3sVd.ygGER8NmUT4uVi/QgZ2752gYYP6E41C', '老王', '234', '女', '1', 0);
-INSERT INTO `user` VALUES (18, 0, '包龙星', '$2a$10$BSqVqr286tURFrOoUJ5ZLuHzoJ30k6xPWDnaR4of12E58bvzar4IK', '包青天', '2212', '男', '2', 0);
-INSERT INTO `user` VALUES (19, 0, '苏乞儿', '$2a$10$W2O5n0Ia3vdsRHA9xwPk3.o1uwVry00OHim3g/9pV/BjBi.EXpPam', '武状元', '11', '男', '0', 0);
 
 SET FOREIGN_KEY_CHECKS = 1;
